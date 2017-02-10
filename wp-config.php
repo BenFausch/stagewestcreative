21b8c5a5a82a10b3ffd5586b656d70a78edd241c
Ben Fausch <ben@digitalbureau.com>
2017-02-10 10:38:34 -0700

    adding private auth

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,12 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'benfausch_stagew');
+require_once('.private-environment-settings.php');
+define('DB_NAME',     ServerEnvironmentSettings::dbName);
+define('DB_USER',     ServerEnvironmentSettings::dbUsername);
+define('DB_PASSWORD', ServerEnvironmentSettings::dbPassword);
+define('DB_HOST',     ServerEnvironmentSettings::dbHost);
 
-/** MySQL database username */
-define('DB_USER', 'benfausch_stagew');
-
-/** MySQL database password */
-define('DB_PASSWORD', 'computer');
-
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
